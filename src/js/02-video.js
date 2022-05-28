6c4413afbc1cb7d6e70afefdd5981cdcfcbd7c79
Nastya Lutsko <nastyalutsko777@gmail.com>
2022-05-28 14:02:58 +0200

    задание 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import {throttle} from "lodash"
+const player = new Player('vimeo-player');
+const inframe = document.querySelector("#vimeo-player")
 
+const onPlay = function (data) {
+   const value = data.seconds;
+   const pauseTime = JSON.stringify(value);
+   localStorage.setItem("videoplayer-current-time", pauseTime);
+    };
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import { throttle } from "lodash"
 
+const form = document.querySelector("form");
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+
+const formInput = (e) => {
+     e.preventDefault();
+    const value = `${email.value}: ${message.value}`
+    localStorage.setItem("feedback-form-state", value);
+}
+form.addEventListener("submit", formInput);
+
+const theme = localStorage.getItem("feedback-form-state");
+console.log(theme);
+
+// function formOutput() {
+    
+// //   storage.addItem("list", result);
+// //     const result =  
+// //     result.push("feedback-form-state", []);
+// }
